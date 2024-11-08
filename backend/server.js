463402b5bc8d7e13f05a03da5ca90b89c42b6ece
muskasaid02 <148300510+muskasaid02@users.noreply.github.com>
2024-11-07 19:54:07 -0800

    confused

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,8 @@ const connectDB = async () => {
         mongoose.set('strictQuery', true);
 
         mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+            //useNewUrlParser: true,
+            //useUnifiedTopology: true
         });
 
         console.log('MongoDB connected');
