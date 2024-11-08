f6cb1a4fc0443da2c257f79d7c24c9ba39ba2325
muskasaid02 <148300510+muskasaid02@users.noreply.github.com>
2024-11-07 22:28:13 -0800

    login working, signup working, logout working, will work on post issues tomorrow

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 
     useEffect(() => {
         const fetchPosts = async () => {
-            const response = await fetch('https://diary-backend-utp0.onrender.com', {
+            const response = await fetch('https://diary-backend-utp0.onrender.com/api/posts', {
                 headers: {
                     'Authorization': `Bearer ${user.token}`
                 }
