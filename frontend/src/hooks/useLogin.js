fbcbc1894528f419ddd4c6679ecd29df8f4b3783
muskasaid02 <148300510+muskasaid02@users.noreply.github.com>
2024-11-07 22:13:04 -0800

    progresssss?

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -14,7 +14,7 @@ export const useLogin = () => {
         setError(null);
     
         try {
-            const response = await fetch('https://diary-backend-utp0.onrender.com', {
+            const response = await fetch('https://diary-backend-utp0.onrender.com/api/user/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
