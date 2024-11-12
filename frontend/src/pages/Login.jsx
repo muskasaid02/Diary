786819981b0d4112145a95f03111b4885f27159d
muskasaid02 <148300510+muskasaid02@users.noreply.github.com>
2024-11-11 18:57:52 -0800

    added temporary image

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,41 +2,58 @@ import { useForm } from "react-hook-form";
 import { useLogin } from "../hooks/useLogin.js";
 import styles from '../styles/styles.module.scss';
 
-
-
 const Login = () => {
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
     const { login, loading, error } = useLogin();
 
     const onSubmit = async data => {
         await login(data.email, data.password);
-        reset({ email: '', password: '' })
+        reset({ email: '', password: '' });
     };
 
     return (
-        <form className={styles.loginForm} onSubmit={handleSubmit(onSubmit)}>
-            <h3> Log In </h3>
-            <input 
-                type="email" 
-                {...register("email", { required: 'required field' })}
-                placeholder="email"
-                autoComplete="off"
-            />
-            <p>{ errors.email?.message }</p>
-            <input 
-                type="password" 
-                {...register("password", { required: 'required field' })}
-                placeholder="password"
-            />
-            <p>{ errors.password?.message }</p>
-            <button 
-                className={styles.submit} type="submit"
-                disabled={loading}
-            > 
-                Log In 
-            </button>
-            {error && <div>{ error }</div>}
-        </form>
+        <div 
+            className={styles['login-container']}
+            style={{
+                backgroundImage: `url(${process.env.PUBLIC_URL}/background.jpg)`, // Set the image path here
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundAttachment: 'fixed',
+                height: '95.5vh',
+                width: '100vw',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                transform: 'translate(-2%, -3.8%)'
+
+            }}
+        >
+            <form className={styles.loginForm} onSubmit={handleSubmit(onSubmit)}>
+                <h3> Log In </h3>
+                <input 
+                    type="email" 
+                    {...register("email", { required: 'required field' })}
+                    placeholder="email"
+                    autoComplete="off"
+                />
+                <p>{ errors.email?.message }</p>
+                <input 
+                    type="password" 
+                    {...register("password", { required: 'required field' })}
+                    placeholder="password"
+                />
+                <p>{ errors.password?.message }</p>
+                <button 
+                    className={styles.submit} 
+                    type="submit"
+                    disabled={loading}
+                > 
+                    Log In 
+                </button>
+                {error && <div>{ error }</div>}
+            </form>
+        </div>
     );
 }
 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -12,31 +12,51 @@ const Signup = () => {
     };
 
     return (
-        <form className={styles.loginForm} onSubmit={handleSubmit(onSubmit)}>
-            <h3> Sign Up </h3>
-            <input 
-                type="email" 
-                {...register("email", { required: 'required field' })}
-                placeholder="email"
-                autoComplete="off"
-            />
-            <p>{ errors.email?.message }</p>
-            <input 
-                type="password" 
-                {...register("password", { required: 'required field' })}
-                placeholder="password"
-            />
-            <p>{ errors.password?.message }</p>
-            <button 
-                className={styles.submit} 
-                type="submit"
-                disabled={loading}
-            > 
-               Sign Up 
-            </button>
-            {error && <p>{ error }</p>}
-        </form>
+        // Background container for signup page
+        <div 
+            className={styles['login-container']}
+            style={{
+                backgroundImage: `url(${process.env.PUBLIC_URL}/background.jpg)`, // Set the image path here
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundAttachment: 'fixed',
+                height: '95.5vh',
+                width: '100vw',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                transform: 'translate(-2%, -3.8%)'
+
+            }}
+        >
+            <form className={styles.loginForm} onSubmit={handleSubmit(onSubmit)}>
+                <h3> Sign Up </h3>
+                <input 
+                    type="email" 
+                    {...register("email", { required: 'required field' })}
+                    placeholder="email"
+                    autoComplete="off"
+                />
+                <p>{ errors.email?.message }</p>
+                <input 
+                    type="password" 
+                    {...register("password", { required: 'required field' })}
+                    placeholder="password"
+                />
+                <p>{ errors.password?.message }</p>
+                <button 
+                    className={styles.submit} 
+                    type="submit"
+                    disabled={loading}
+                > 
+                   Sign Up 
+                </button>
+                {error && <p>{ error }</p>}
+            </form>
+        </div>
     );
 }
 
 export default Signup;
+
