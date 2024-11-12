f56b2eafab1a3ed103d7f12cf98aaa4a02b71446
muskasaid02 <148300510+muskasaid02@users.noreply.github.com>
2024-11-11 20:20:46 -0800

    is this better? should fit the entire screen

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useAuthContext } from '../hooks/useAuthContext';
 import { useLogout } from '../hooks/useLogout.js';
+import styles from '../styles/styles.module.scss';
 
 const NavBar = () => {
     const { user } = useAuthContext();
@@ -9,7 +10,7 @@ const NavBar = () => {
     const handleClick = () => logout();
 
     return (
-        <header>
+        <header className={styles.navbar}>
             <nav>
                 <ul>
                     <li> 
@@ -18,7 +19,7 @@ const NavBar = () => {
                 </ul>
             </nav>
 
-            <span> The Diary App </span>
+            <span className={styles['navbar-brand']}> The Diary App </span>
 
             {user ? (
                 <div>
@@ -27,7 +28,6 @@ const NavBar = () => {
                         Logout
                     </button>
                 </div>
-
             ) : (
                 <div>
                     <Link to="/api/login"> Login </Link>
