b2d072770cb0cd574919eb82768c8579b2f78702
muskasaid02 <148300510+muskasaid02@users.noreply.github.com>
2024-12-09 18:34:38 -0800

    i think backend is the same now?

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
 const PostSchema = new Schema({
     date: {
@@ -19,11 +20,36 @@ const PostSchema = new Schema({
 
     user_id: {
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'User',
+        required: true
+    },
+    mood: {
+        type: String,
+        required: false, // Optional
+        enum: ['happy', 'sad', 'excited', 'anxious', 'neutral'], // Define allowed moods
+        default: 'neutral'
+    },
+
+    password: {
+        type: String,
+        required: false
     }
+
 }, {
     collection: 'posts',
     timestamps: true
+});
+
+PostSchema.pre('save', async function (next) {
+    if (this.password && this.isModified('password')) {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            this.password = await bcrypt.hash(this.password, salt);
+        } catch (err) {
+            return next(err);
+        }
+    }
+    next();
 });
 
 export default model('Post', PostSchema);
