3a2b52a30a045984e0c2c6e10b98c5cffb6a7a32
Muska Said <148300510+muskasaid02@users.noreply.github.com>
2025-02-03 16:14:03 -0800

    Calendar view (#105)
    * frontend for calendarView
    
    * changed ports to test
    
    * needa fix backend up:
    
    * changed ports for render testing
    
    ---------
    
    Co-authored-by: Pranav Nallaperumal <pnallape@calpoly.edu>

---
+++ frontend/src/constants/moodColors.js
@@ -0,0 +1,9 @@
+export const moodColors = {
+    Happy: "#FFD700", // Yellow
+    Sad: "#1E90FF",   // Blue
+    Excited: "#FF4500", // Red
+    Anxious: "#800080", // Purple
+    Neutral: "#808080"  // Gray
+  };
+
+  
